85a1841548fcdd5f7a9e77b3b283edbad2193180
Plywacz <=>
2019-10-22 21:58:54 +0200

    created file reader

---
+++ FirstScenarioIO/src/main/java/FileReader.java
@@ -0,0 +1,74 @@
+/*
+Author: Marcin Plywacz
+Date: 22.10.2019
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/***
+ * Reads all files  with DEMANDED_EXTENSION from given directory and it's subdirectories
+ */
+public class FileReader {
+    public static final String DEMANDED_EXTENSION = ".java";
+
+    private final List<File> fileList = new ArrayList<>();
+    private final Queue<File> directoriesQueue = new LinkedList<>();
+    private final File primaryDir; //folder from where we start search for files and subdirectories
+
+    public FileReader(String primaryDirPath) throws IOException {
+        File tmpFile = new File(primaryDirPath);
+        if (!tmpFile.exists()) {
+            throw new FileNotFoundException("cannot load given directory");
+        }
+        else if (!tmpFile.canRead()) {
+            throw new IOException("cannot read directory");
+        }
+        else if (!tmpFile.isDirectory()) {
+            throw new IllegalArgumentException("given path should lead to directory ont file ");
+        }
+        primaryDir = tmpFile;
+        findAllJavaFilesInDepth();
+    }
+
+    /**
+     * fills in fileList  with files that ends with DEMANDED_EXTENSION
+     */
+    private void findAllJavaFilesInDepth() {
+        directoriesQueue.add(primaryDir);
+        while (!directoriesQueue.isEmpty()) {
+            File currentDir = directoriesQueue.remove();
+            File[] currentFiles = currentDir.listFiles();
+            if (currentFiles == null) {
+                continue;
+            }
+            for (File file : currentFiles) {
+                if (file.isDirectory()) {
+                    directoriesQueue.add(file);
+                }
+                else if (file.getName().endsWith(DEMANDED_EXTENSION)) {
+                    fileList.add(file);
+                }
+            }
+        }
+    }
+
+    public List<File> getFileList() {
+        return fileList;
+    }
+
+    public File getPrimaryDir() {
+        return primaryDir;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new FileReader("D:\\Studia\\sem_5\\IO\\first_scenario\\FirstScenarioIO");
+    }
+
+
+}
