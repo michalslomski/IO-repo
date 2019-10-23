6d93267c955a78661a1db51627a67cc324b760ce
miProchwicz <56887831+kbenoo@users.noreply.github.com>
2019-10-23 17:57:58 +0200

    Update Node.java

--- FirstScenarioIO/src/main/java/Node.java
+++ FirstScenarioIO/src/main/java/Node.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.HashMap;
 
 public class Node {
-    int fileSize;               			//pobrać z FileParser
+    long fileSize;               			//pobrać z FileParser
     File file;
     HashMap<String,Integer> connectionsWeights;    	//key: className, value: weight of connection
 
