6926d80d15f07255ef666c78d846dd70ed2f278c
Plywacz <=>
2019-10-24 12:21:20 +0200

    class doesnt compile

--- FirstScenarioIO/src/main/java/GraphFrame.java
+++ FirstScenarioIO/src/main/java/GraphFrame.java
@@ -4,16 +4,16 @@ import javax.swing.*;
 public class GraphFrame extends JFrame{
 
     public GraphFrame(){
-        super("Graph Viever");
-
-        JPanel display = new GraphPanel(this);
-        display.setSize(600,600);
-        add(display);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-
-        setVisible(true);
+//        super("Graph Viever");
+//
+//        JPanel display = new GraphPanel(this);
+//        display.setSize(600,600);
+//        add(display);
+//
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        pack();
+//
+//        setVisible(true);
 
     }
 }
