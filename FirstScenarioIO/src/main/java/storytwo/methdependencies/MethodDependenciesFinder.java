007d16f7b54e5316eb35a18bf3413b59c069dc8c
Plywacz <=>
2019-11-08 21:17:29 +0100

    added classes for method dependency finding and updated pom file to add necessary libraries

---
+++ FirstScenarioIO/src/main/java/storytwo/methdependencies/MethodDependenciesFinder.java
@@ -0,0 +1,76 @@
+/**
+ * Author: Marcin Plywacz
+ * Date: 07.11.2019
+ */
+
+
+package storytwo.methdependencies;
+
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.utils.SourceRoot;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+public class MethodDependenciesFinder {
+    /**
+     * Method scans given directory for classes and finds every method
+     * declared in those classes. Then extracts every inside method called from ours class'es methods.
+     * then makes dependencies
+     *
+     * @param dirPath path to directory to be scanned
+     * @return list of dependencies. See MethodDependency class
+     */
+    public List<MethodDependency> getMethodDependencies(String dirPath) {
+        //root is folder where we start scanning for classes
+        SourceRoot root = new SourceRoot(Paths.get(dirPath));
+        List<ParseResult<CompilationUnit>> parsingResults = root.tryToParseParallelized();
+
+        //we get every compiled unit for existing class
+        List<CompilationUnit> compilationUnitList =
+                parsingResults
+                        .stream()
+                        .map(ParseResult::getResult)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .collect(Collectors.toList());
+
+        //finds all defined methods from classes that are in root folder.
+        List<MethodDeclaration> pathMethodsList = new ArrayList<>();
+        for (CompilationUnit cu : compilationUnitList) {
+            for (TypeDeclaration typeDeclaration : cu.getTypes()) {
+                List<BodyDeclaration> bodyDeclarations = typeDeclaration.getMembers();
+                for (BodyDeclaration bodyDeclaration : bodyDeclarations) {
+                    if (!bodyDeclaration.isMethodDeclaration())
+                        continue;
+                    MethodDeclaration methodDeclaration = (MethodDeclaration) bodyDeclaration;
+                    pathMethodsList.add(methodDeclaration);
+                }
+            }
+        }
+
+        List<MethodDependency> methodDependencies = new LinkedList<>();
+        //for every method from root folder extract every called method and add to dependencies list
+        pathMethodsList.forEach(method -> {
+            MethodDependency methodDep = new MethodDependency(method.getNameAsString());
+
+            method.walk(MethodCallExpr.class, methodInside -> {
+                methodDep.addDependency(methodInside.getNameAsString());
+            });
+            methodDependencies.add(methodDep);
+        });
+
+
+        return methodDependencies;
+    }
+}
---
+++ FirstScenarioIO/src/main/java/storytwo/methdependencies/MethodDependency.java
@@ -0,0 +1,65 @@
+/**
+ * Author: Marcin Plywacz
+ * Date: 07.11.2019
+ */
+
+package storytwo.methdependencies;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * callingMethodName : nazwa metody dla ktorej ta klasa zawiera zaleznosci
+ * dependencyMap: mapa ktora zawiera nazwy wywolanych metod jako klucze,
+ *  liczbe razy jako wartosci
+ *
+ * kazdy klucz mapy oznacza nazwe metody ktora byla wywolana przez metode wywolujaca,
+ * wartosc klucza to liczba razy
+ */
+public class MethodDependency {
+    /**
+     * contains name of method to which belongs dependencies
+     */
+    private String callingMethodName;
+    /**
+     * contains dependencies for calling method,
+     * keys are names of called methods from callingMethodName's body
+     * values are numbers meaning how many times
+     * <p>
+     * format of the dependencyMap:
+     * "name of called method" -> how many times
+     */
+    private Map<String, Integer> dependencyMap = new HashMap<>();
+
+    public MethodDependency(String methodName) {
+        this.callingMethodName = methodName;
+    }
+
+    public void addDependency(String dependency) {
+        dependencyMap.merge(dependency, 1, Integer::sum);
+        //above code does the same as below code
+//        Integer count = dependencyMap.get(dependency);
+//        if (count == null) {
+//            dependencyMap.put(dependency, 1);
+//        }
+//        else
+//            dependencyMap.put(dependency, count + 1);1
+
+    }
+
+    public String getCallingMethodName() {
+        return callingMethodName;
+    }
+
+    public Map<String, Integer> getDependencyMap() {
+        return dependencyMap;
+    }
+
+    @Override
+    public String toString() {
+        return "MethodDependency{" +
+                "callingMethodName='" + callingMethodName + '\'' +
+                ", dependencyMap=" + dependencyMap +
+                '}';
+    }
+}
