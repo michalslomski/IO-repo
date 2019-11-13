59c64680996325830ee861a29730e5621b672952
miProchwicz <56887831+kbenoo@users.noreply.github.com>
2019-11-14 00:02:00 +0100

    Update GraphMaker.java
    Dodano wyświetlanie rozmiaru pliku w vertexie grafu

--- FirstScenarioIO/src/main/java/storyone/graph/GraphMaker.java
+++ FirstScenarioIO/src/main/java/storyone/graph/GraphMaker.java
@@ -1,60 +1,50 @@
 package storyone.graph;
 
 import org.jgrapht.graph.DirectedWeightedMultigraph;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
-public class GraphMaker {		//aka GraphDraw
-    //https://stackoverflow.com/questions/20246409/how-to-include-weight-in-edge-of-graph
-    //stąd wiem jak można robic graf
+public class GraphMaker {		
+     private HashMap<String,String> vertexMap= new HashMap<>();          //mapa unikalnych, niepowtarzających się węzłów/wierzchołków grafu
+  
+    //https://stackoverflow.com/questions/20246409/how-to-include-weight-in-edge-of-graph           , stąd wiadomo jak można robic graf
     public DirectedWeightedMultigraph<String, CustomEdge> makeGraph(List<Node> sourceVertexList) {
-        DirectedWeightedMultigraph<String, CustomEdge> graph = new DirectedWeightedMultigraph<String, CustomEdge>(CustomEdge.class);
+        DirectedWeightedMultigraph<CustomVertex, CustomEdge> graph = new DirectedWeightedMultigraph<CustomVertex, CustomEdge>(CustomEdge.class);
 
-        //dodawanie wyłącznie węzłów
-        for (int i = 0; i < sourceVertexList.size(); i++) {
-            Node n = sourceVertexList.get(i);
-            String sourceVortex = n.file.getName();
-
-            graph.addVertex(sourceVortex);          //węzły które odpowiadają plikom w projekcie
-
-            //
+        //uzupełnianie vertexMap
+        for (Node n : sourceVertexList) {
+            vertexMap.put(n.file.getName(), Objects.toString(n.fileSize) );          //węzły które odpowiadają plikom w projekcie
+            
             for (Object key : n.dependencies.keySet().toArray()) {
-                if (!graph.containsVertex(key.toString()))
-                    graph.addVertex(key.toString());            //węzły które odpowiadają plikom importowanym
-            }                                                   //!!!!!!tzn. że nie wszystkie węzły pokazane na grafie zostały utworzone z klasy storyone.graph.Node
-                                                                //chodzi własnie o te tworzone w tej pętli
+                if (!vertexMap.containsKey(key.toString()))
+                    vertexMap.put(key.toString(), "");                  //węzły które odpowiadają plikom importowanym, nieznane są wielkości tych plików, stąd ""
+            }  //chodzi o te tworzone w tej pętli                       //!!!tzn. że nie wszystkie węzły/wierzcholki pokazane na grafie zostały utworzone z klasy storyone.graph.Node
+            
+        }
+
+        //dodawanie węzłów do grafu z vertexMap
+        for (String key :vertexMap.keySet()) {
+            CustomVertex sourceVortex = new CustomVertex(key,vertexMap.get(key));
+            graph.addVertex(sourceVortex);
         }
 
         //tworzenie połącznień w grafie
         //dla każdego Noda z listy
         for (int i = 0; i < sourceVertexList.size(); i++) {
             Node n = sourceVertexList.get(i);
-            String sourceVortex = n.file.getName();
+            CustomVertex sourceVortex = new CustomVertex(n.file.getName(),Objects.toString(n.fileSize));       //CustomVertex(Nazwa pliku w wierzcholku, rozmiar pliku)
             System.out.println(sourceVortex);
 
             //dla każdej zależności w konkretnym Nodzie
-            for (Object key : n.dependencies.keySet().toArray()) {
-                String targetVortex = key.toString();
-                Integer weight = n.dependencies.get(targetVortex);
+            for (String fileName : n.dependencies.keySet()) {
+                CustomVertex targetVortex = new CustomVertex(fileName,vertexMap.get(fileName));
+                Integer weight = n.dependencies.get(fileName);
 
                 if (!sourceVortex.equals(targetVortex) && weight != 0) {
                     graph.addEdge(sourceVortex, targetVortex, new CustomEdge(weight.toString()));
                 }
             }
-
-            /*
-            for (int j = 0; j < n.dependencies.size(); j++) {
-                String targetVortex = n.dependencies.keySet().toArray()[j].toString();
-                Integer weight = n.dependencies.get(targetVortex);
-
-
-                System.out.println("////////////////////////////////");
-                System.out.println(n.dependencies.keySet().toArray()[j].toString());
-
-                if (!sourceVortex.equals(targetVortex) && weight != 0) {
-                    storyone.graph.addEdge(sourceVortex, targetVortex, new storyone.graph.CustomEdge(weight.toString()));
-                }
-            }*/
-
         }
         return graph;
     }
