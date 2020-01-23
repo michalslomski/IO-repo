e883ce8806b64a688b526c784097c16b6dc97c7b
miProchwicz <56887831+kbenoo@users.noreply.github.com>
2020-01-23 18:56:29 +0100

    Update Main.java

--- FirstScenarioIO/src/main/java/mainpackage/Main.java
+++ FirstScenarioIO/src/main/java/mainpackage/Main.java
@@ -1,15 +1,19 @@
 package mainpackage;
 
+import storyfive.run.StoryFive;
+import storyone.run.StoryOne;
 import storythree.run.StoryThree;
+import storytwo.run.StoryTwo;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        //StoryOne.runFirstStory();
-        //StoryTwo.runSecondStory();
+        //stworzone grafy znajdują sie w /src/main/resources
+        StoryOne.runFirstStory();
+        StoryTwo.runSecondStory();
         StoryThree.runThirdStory();
+        StoryFive.runFifthStory();
     }
 }
